58313ad6b5cf180247d767e49fdadf24269d6ab7
AdityaDimri1 <dimriaditya1@gmail.com>
2023-12-26 13:32:21 +0530

    Count Frequence of a word in a file

---
+++ Word_count.py
@@ -0,0 +1,9 @@
+a = input('Enter the file name : ')
+try:
+    b = open(a)
+    c = b.read()
+except:
+    print('Enter a valid file name.')
+    exit()
+occurence = c.count(input('Enter the word you want :'))
+print('No of occurence of the word is :',occurence)

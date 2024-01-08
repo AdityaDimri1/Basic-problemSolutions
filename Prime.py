9949675206b0be519004f68b65fd95b983dd4827
AdityaDimri1 <dimriaditya1@gmail.com>
2024-01-08 17:48:02 +0530

    New branch

--- Prime.py
+++ Prime.py
@@ -1,9 +1,24 @@
-a = int(input('Enter the number : '))
-if(a==2 or a==3):
-    print('Prime')
-    exit()
-for i in range(2,int(a/2)+1):
-    if(a%i==0):
-        print('Not prime')
-        exit()
-print('Prime')
+# a = int(input('Enter the number : '))
+# if(a==2 or a==3):
+#     print('Prime')
+#     exit()
+# for i in range(2,int(a/2)+1):
+#     if(a%i==0):
+#         print('Not prime')
+#         exit()
+# print('Prime')
+
+
+# 1st
+# InputNumber=int(input('Enter the number : '))
+# for i in range(0,InputNumber):
+#     print(i*i)
+
+# 2nd
+
+
+
+# 3rd
+# InputName=input('Enter your name:')
+# Capitalize=a.title()
+# print(Capitalize)

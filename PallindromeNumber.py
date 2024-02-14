1faaa25453fe897b1ce0c16795d91455b088d42c
AdityaDimri1 <dimriaditya1@gmail.com>
2024-02-14 18:13:45 +0530

    Pallindrome Number Solutions

--- PallindromeNumber.py
+++ PallindromeNumber.py
@@ -1,11 +1,9 @@
 a=input('Enter Number : ')
 if a==a[::-1]:
-
     print('Yes',a,'is pallindrome number.')          #Pallindrome number
 else:
     print('No',a,'is not pallindrome number.')
 
-    print('Yes',a,'is a pallindrome number.')          
-else:
-    print('No',a,'is not a pallindrome number.')
+   
+
 

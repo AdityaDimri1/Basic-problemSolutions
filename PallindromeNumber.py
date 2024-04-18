e9e6ef761b323f7f00786513ba07afdd691fe8ed
AdityaDimri1 <dimriaditya1@gmail.com>
2024-04-18 19:43:35 +0530

    Find the number is pallindrome or not

--- PallindromeNumber.py
+++ PallindromeNumber.py
@@ -1,9 +1,16 @@
-a=input('Enter Number : ')
-if a==a[::-1]:
-    print('Yes',a,'is pallindrome number.')          #Pallindrome number
-else:
-    print('No',a,'is not pallindrome number.')
+inputNumber=int(input('Enter Number : '))
 
-   
+def reverse_number(inputNumber):                            #reversing the number
+    reverse=0
+    while inputNumber>0:
+        remainder=inputNumber%10
+        reverse=(reverse*10)+remainder
+        inputNumber=inputNumber//10
+    return reverse
+
+if inputNumber == reverse_number(inputNumber):
+    print('Yes',inputNumber,'is pallindrome number.')          # Pallindrome number
+else:
+    print('No',inputNumber,'is not pallindrome number.')
 
 
